2d2182ffcc9e45e7d4fc63d2cb0bf98978f4d3ee
Shengnan-Zhu <zhu_shengnan@qq.com>
2025-02-08 14:22:49 +0800

    autocast infer

--- video_depth_anything/video_depth.py
+++ video_depth_anything/video_depth.py
@@ -102,8 +102,10 @@ class VideoDepthAnything(nn.Module):
                 cur_input[:, :OVERLAP, ...] = pre_input[:, KEYFRAMES, ...]
 
             with torch.no_grad():
-                depth = self.forward(cur_input) # depth shape: [1, T, H, W]
+                with torch.autocast(device_type=device, enabled=True):
+                    depth = self.forward(cur_input) # depth shape: [1, T, H, W]
 
+            depth = depth.to(cur_input.dtype)
             depth = F.interpolate(depth.flatten(0,1).unsqueeze(1), size=(frame_height, frame_width), mode='bilinear', align_corners=True)
             depth_list += [depth[i][0].cpu().numpy() for i in range(depth.shape[0])]
 
